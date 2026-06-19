efd31e1ace4cf7a64ea37e9d4d00f4c426fd90d1
Alfi2a <a7agam@gmail.com>
2026-06-19 20:41:00 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
+function checkAnswer(correctAnswer, redirectUrl) {
+    const input = document.getElementById("answer").value;
+    const message = document.getElementById("message");
 
+    if (input === correctAnswer) {
+        window.location.href = redirectUrl;
+    } else {
+        message.textContent = "Wrong answer.";
+    }
+}
+
+document.getElementById("submitBtn").addEventListener("click", () => {
+    checkAnswer("⛄︎", "nil.html");
+});
